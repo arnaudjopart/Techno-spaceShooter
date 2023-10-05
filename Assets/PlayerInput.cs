b761bf7870ec4b996fb092d4363c048f230d10d1
Arnaud Jopart <arnaud.jopart@gmail.com>
2023-10-05 09:30:00 +0200

    intro

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -2,28 +2,24 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    public InputListenerBase m_inputListener;
-    void Start()
-    {
-        
-    }
+    public PlayerShip m_playerShip;
 
     // Update is called once per frame
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         var mousePosition = Input.mousePosition;
-        
-        m_inputListener.ProcessMousePosition(mousePosition);
-        m_inputListener.ProcessInputAxes(input);
+
+        m_playerShip.ProcessMousePosition(mousePosition);
+        m_playerShip.ProcessInputAxes(input);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_inputListener.ProcessKeyCodeDown(KeyCode.Space);
+            m_playerShip.ProcessKeyCodeDown(KeyCode.Space);
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            m_inputListener.ProcessKeyCodeUp(KeyCode.Space);
+            m_playerShip.ProcessKeyCodeUp(KeyCode.Space);
         }
     }
 }
--- Assets/PlayerShip.cs
+++ Assets/PlayerShip.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerShip : InputListenerBase
+public class PlayerShip : MonoBehaviour
 {
     private Rigidbody2D m_rigidBody;
     [SerializeField] private float m_impulse;
@@ -12,9 +12,24 @@ public class PlayerShip : InputListenerBase
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
     }
-    public override void ProcessInputAxes(Vector2 input)
+    public void ProcessInputAxes(Vector2 input)
     {
         m_rigidBody.AddForce(transform.up * m_impulse*input.y);
         transform.rotation *= Quaternion.Euler(0, 0, input.x*m_rotationSpeed*Time.deltaTime);
+    }
+
+    internal void ProcessMousePosition(Vector3 mousePosition)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void ProcessKeyCodeDown(KeyCode space)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void ProcessKeyCodeUp(KeyCode space)
+    {
+        throw new NotImplementedException();
     }
 }

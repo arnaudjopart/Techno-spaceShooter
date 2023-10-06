9645c5ed7ef3fa4f83c71704cce911a9f2bcc164
tranKILL.be <pit190@gmail.com>
2023-10-06 09:47:21 +0200

    Refactoring projectile

--- Assets/Script_NJ/PlayerShip_NJ.cs
+++ Assets/Script_NJ/PlayerShip_NJ.cs
@@ -19,6 +19,7 @@ public class PlayerShip_NJ : InputListenerBase
     //private List<Projectile> projectiles = new List<Projectile>();
     private Vector2 _mousePositionAtFrame;
     private bool tireLazerActif = false;
+    private GameObject newProjectile;
 
     private void Awake()
     {
@@ -46,7 +47,7 @@ public class PlayerShip_NJ : InputListenerBase
         for (int i = 0; i < maxProjectiles; i++)
         {
             //Projectile newProjectile = Instantiate(projectilePrefab, Vector3.zero, Quaternion.identity);
-            GameObject newProjectile = Instantiate(projectilePrefab, Vector3.zero, Quaternion.identity);
+            newProjectile = Instantiate(projectilePrefab, Vector3.zero, Quaternion.identity);
             newProjectile.SetActive(false);
             //newProjectile.OnCollision += HandleProjectileCollision;
             projectiles.Add(newProjectile);
@@ -90,7 +91,7 @@ public class PlayerShip_NJ : InputListenerBase
         if (_button == 0) // tire simple
         {
             //Projectile newProjectile = GetInactiveProjectile();
-            GameObject newProjectile = GetInactiveProjectile();
+            newProjectile = GetInactiveProjectile();
 
             if (newProjectile != null)
             {
@@ -108,7 +109,7 @@ public class PlayerShip_NJ : InputListenerBase
         else {
             //tire laser en laissant appuyer ? -> ProcessMouseButtonUp + bool
             tireLazerActif = true;
-            GameObject newProjectile = GetInactiveProjectile();
+            newProjectile = GetInactiveProjectile();
             if (newProjectile != null)
             {
                 newProjectile.transform.position = transform.position;

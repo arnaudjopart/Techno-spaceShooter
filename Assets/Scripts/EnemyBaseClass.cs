a2d25d1a985a4068d935b0e4a04460250f16d7cd
Arnaud Jopart <arnaudjopart@users.noreply.github.com>
2023-10-06 15:48:23 +0200

    Adding effects

--- Assets/Scripts/EnemyBaseClass.cs
+++ Assets/Scripts/EnemyBaseClass.cs
@@ -6,21 +6,40 @@ using UnityEngine;
 public class EnemyBaseClass : MonoBehaviour, IMovable
 {
     [SerializeField] protected int m_lives;
-    IEnumerator m_freezeCoroutine;
+    IEnumerator m_cachedCoroutine;
     [SerializeField] private float m_speed;
 
     public float Speed => m_speed;
 
     internal void FreezeForSeconds(float m_freezeTime)
     {
-        m_freezeCoroutine = FreezeCoroutine(m_freezeTime);
-        StartCoroutine(m_freezeCoroutine);
+        m_cachedCoroutine = FreezeCoroutine(m_freezeTime);
+        StartCoroutine(m_cachedCoroutine);
+    }
+
+    internal void ReduceSize()
+    {
+        transform.localScale = Vector3.one * .5f;
+    }
+
+    internal void ReduceSpeed(float _time, float _speedFactor)
+    {
+        m_cachedCoroutine = ReduceSpeedForSeconds(_time, _speedFactor);
+        StartCoroutine(m_cachedCoroutine);
     }
 
     private IEnumerator FreezeCoroutine(float _freezeTime)
     {
         var currentSpeed = m_speed;
         m_speed = 0;
+        yield return new WaitForSeconds(_freezeTime);
+        m_speed = currentSpeed;
+    }
+
+    private IEnumerator ReduceSpeedForSeconds(float _freezeTime, float _speedFactor)
+    {
+        var currentSpeed = m_speed;
+        m_speed *= _speedFactor;
         yield return new WaitForSeconds(_freezeTime);
         m_speed = currentSpeed;
     }

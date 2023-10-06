79002d29ad67c56194eba14cd944b2c16463d2dc
Geoffrey Verlaine <geoffrey.verlaine@hotmail.com>
2023-10-06 14:58:43 +0200

    SpaceShipGeo

---
+++ Assets/Geoffrey/Scripts/ScreenWrap.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ScreenWrap : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        float rightSideOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height)).x;
+        float leftSideOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector2(0f,0f)).x;
+
+        float topOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).y;
+        float bottomOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector2(0f,0f)).y;
+
+        if (screenPos.x <= 0 && myRigidBody.velocity.x < 0)
+        {
+            transform.position = new Vector2(rightSideOfScreenInWorld, transform.position.y);
+        }
+    }
+}

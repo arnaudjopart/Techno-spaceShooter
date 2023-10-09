673fa24479f1457d953d37027eca0ef4dea42a90
Scephiroth <144216601+Scephiroth@users.noreply.github.com>
2023-10-09 09:45:49 +0200

    Asteroid blow

--- Assets/AsteroidHit.cs
+++ Assets/AsteroidHit.cs
@@ -5,37 +5,57 @@ using UnityEngine;
 public class AsteroidHit : MonoBehaviour
 {
     [SerializeField] GameObject debrisPrefab;
-    [SerializeField] Rigidbody rb;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float explosionForceMin;
     [SerializeField] float explosionForceMax;
     float explosionForce;
-    [SerializeField] float angleForceMin;
+
     [SerializeField] float angleForceMax;
     float angleForce;
+    [SerializeField] float horizontalOffset;
+    [SerializeField] float verticalOffset;
+    Vector3 offset;
 
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        explosionForce = Random.Range(0, explosionForceMax);
-        angleForce = Random.Range(angleForceMin, angleForceMax);
+        offset.x = Random.Range(-horizontalOffset, horizontalOffset);
+        offset.y = Random.Range(-verticalOffset, verticalOffset);
+        offset.z = 0;
+        rb = GetComponent<Rigidbody2D>();
+        explosionForce = Random.Range(explosionForceMin, explosionForceMax);
+        angleForce = Random.Range(-angleForceMax, angleForceMax);
     }
 
     public void Explode()
     {
+        Debug.Log("destroy");
         Destroy(gameObject);
     }
 
     public void SpawnDebris()
     {
+        Vector3 direction = rb.velocity;
+        Debug.Log("velocity = " + direction);
         for (int i = 0; i<2;  i++)
         {
-            Vector3 direction = rb.velocity;
-            GameObject debris = Instantiate(debrisPrefab, transform.position, transform.rotation);
-            angleForce = Random.Range(angleForceMin, angleForceMax);
+            
+            offset.x = Random.Range(-horizontalOffset, horizontalOffset);
+            offset.y = Random.Range(-verticalOffset, verticalOffset);
+            offset.z = 0;
+            GameObject debris = Instantiate(debrisPrefab, transform.position+ offset, transform.rotation);
+            angleForce = Random.Range(-angleForceMax, angleForceMax);
             Vector3 newDirection = Quaternion.Euler(0,0,angleForce) *direction ;
-            debris.GetComponent<Rigidbody>().AddForce(direction* explosionForce);
+            Debug.Log("new direction = " + newDirection);
+            debris.GetComponent<Rigidbody2D>().AddForce(newDirection* explosionForce, ForceMode2D.Impulse);
         }
     }
 
- 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        SpawnDebris();
+        Explode();
+    }
+
+
 }

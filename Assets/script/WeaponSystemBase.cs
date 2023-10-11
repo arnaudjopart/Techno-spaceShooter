c0c04edec384fad5ba118b8595f92735c14f4436
Arnaud Jopart <arnaudjopart@users.noreply.github.com>
2023-10-11 12:29:04 +0200

    WeaponSystemBase now in a dedicated cs file

---
+++ Assets/script/WeaponSystemBase.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+internal abstract class WeaponSystemBase : MonoBehaviour
+{
+    public abstract void ProcessShootPrimaryWeapon();
+    public abstract void ProcessShootSecondaryWeapon();
+}
